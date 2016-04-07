2e5b5bed90258923c9210a87e19374d5001d1730
Gavin Joyce <gavinjoyce@gmail.com>
2016-04-07 09:55:20 +0100

    fixup!

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,27 @@
 import Em from 'ember';
 
 export default Em.Route.extend({
+  model() {
+    console.log('Em.assert');
+    Em.assert('thiswillfailindev');
+
+    console.log('Em.debug');
+    Em.debug('debug')
+
+    console.log('Em.deprecate');
+    Em.deprecate('deprecate')
+
+    // console.log('Em.info');
+    // Em.info('info')
+
+    Em.runInDebug(() => {
+      console.log('this is debug!');
+    });
+
+    console.log('Em.warn');
+    Em.warn('warn')
+
+  },
   actions: {
     transitionTo: function(name) {
       this.transitionTo(name);
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,38 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var filterImports = require('babel-plugin-filter-imports');
+
+function babelConfigFor(environment) {
+  var isDevelopment = (environment === 'development');
+  var isProduction = (environment === 'production');
+
+  var plugins = [];
+
+  if (isProduction) {
+    plugins.push(filterImports({
+      'ember-metal/debug': ['assert', 'debug', 'deprecate', 'info', 'runInDebug', 'warn', 'debugSeal']
+    }));
+  }
+
+  return { plugins: plugins };
+}
+
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    babel: {
+      plugins: [
+        filterImports({
+          'ember': ['assert', 'debug', 'deprecate', 'info', 'runInDebug', 'warn', 'debugSeal']
+        })
+      ]
+      // development: babelConfigFor('development'),
+      // production: babelConfigFor('production')
+    }
   });
+
+  // console.log('app', app);
 
   // Use `app.import` to add additional libraries to the generated
   // output files.

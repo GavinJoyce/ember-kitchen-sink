6fc555a8315bfe738a08bea8f256972f2803108d
Gavin Joyce <gavinjoyce@gmail.com>
2016-04-21 13:07:03 +0100

    mock Em.run.later

--- tests/integration/components/delayed-action-button-test.js
+++ tests/integration/components/delayed-action-button-test.js
@@ -7,22 +7,49 @@ moduleForComponent('delayed-action-button', 'Integration | Component | delayed a
   integration: true
 });
 
-test('should change text after a delay', function(assert) {
+//this test uses a longer `Em.run.later`
+test('option 1: should change text after a delay', function(assert) {
   assert.expect(3);
 
   this.render(hbs`{{delayed-action-button}}`);
 
   assert.equal(this.$('button').text(), 'Click me');
 
-  Em.run(() => {
-    this.$('button').click();
-  });
+  Em.run(() => this.$('button').click());
 
   assert.equal(this.$('button').text(), '...please wait...');
 
   Em.run.later(() => {
     assert.equal(this.$('button').text(), 'Click me again');
   }, 1050);
+
+  return wait();
+});
+
+//this test mocks `Em.run.later`
+test('option 2: should change text after a delay', function(assert) {
+  assert.expect(3);
+
+  this.render(hbs`{{delayed-action-button}}`);
+
+  assert.equal(this.$('button').text(), 'Click me');
+
+  let originalRunLater = Em.run.later;
+  let laterDefer;
+  Em.run.later = function(fn) {
+    laterDefer = Em.RSVP.defer();
+    laterDefer.promise.then(fn);
+  };
+
+  Em.run(() => this.$('button').click());
+
+  assert.equal(this.$('button').text(), '...please wait...');
+
+  Em.run(() => laterDefer.resolve());
+
+  assert.equal(this.$('button').text(), 'Click me again');
+
+  Em.run.later = originalRunLater;
 
   return wait();
 });

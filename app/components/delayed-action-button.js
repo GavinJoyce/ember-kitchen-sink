efae65198e78f2d108bb8f1e88a1ee164d5976bb
Gavin Joyce <gavinjoyce@gmail.com>
2016-04-21 14:02:28 +0100

    support multiple laters

--- app/components/delayed-action-button.js
+++ app/components/delayed-action-button.js
@@ -5,7 +5,9 @@ export default Em.Component.extend({
   buttonText: 'Click me',
   buttonPressed: task(function * () {
     this.set('buttonText', '...please wait...');
-    yield timeout(1000);
+    yield timeout(1001);
+    this.set('buttonText', '...continue to wait...');
+    yield timeout(1002);
     this.set('buttonText', 'Click me again');
   }).drop()
 });
--- tests/integration/components/delayed-action-button-test.js
+++ tests/integration/components/delayed-action-button-test.js
@@ -1,55 +1,84 @@
 import Em from 'ember';
 import { test, moduleForComponent } from 'ember-qunit';
-import wait from 'ember-test-helpers/wait';
 import hbs from 'htmlbars-inline-precompile';
+
+function mockRunLater(context) {
+  context.mockRunLater = { //TODO: GJ: naming
+    originalRunLater: Em.run.later,
+    defferedRunLaters: [],
+    next: function() {
+      Em.run(() => {
+        let defered = context.mockRunLater.defferedRunLaters.splice(0, 1)[0];
+
+        if(defered) {
+          defered.resolve();
+        } else {
+          throw "there are no more defered run laters";
+        }
+      });
+    }
+  };
+
+  Em.run.later = function(fn) {
+    let defer = Em.RSVP.defer();
+    defer.promise.then(fn);
+    context.mockRunLater.defferedRunLaters.push(defer);
+  };
+}
+
+function restoreRunLater(context) {
+  Em.run.later = context.mockRunLater.originalRunLater;
+}
 
 moduleForComponent('delayed-action-button', 'Integration | Component | delayed action button', {
   integration: true
 });
 
-//this test uses a longer `Em.run.later`
-test('option 1: should change text after a delay', function(assert) {
-  assert.expect(3);
-
-  this.render(hbs`{{delayed-action-button}}`);
-
-  assert.equal(this.$('button').text(), 'Click me');
-
-  Em.run(() => this.$('button').click());
-
-  assert.equal(this.$('button').text(), '...please wait...');
-
-  Em.run.later(() => {
-    assert.equal(this.$('button').text(), 'Click me again');
-  }, 1050);
-
-  return wait();
-});
-
 //this test mocks `Em.run.later`
 test('option 2: should change text after a delay', function(assert) {
-  assert.expect(3);
+  mockRunLater(this);
+
+  assert.expect(4);
 
   this.render(hbs`{{delayed-action-button}}`);
 
   assert.equal(this.$('button').text(), 'Click me');
 
-  let originalRunLater = Em.run.later;
-  let laterDefer;
-  Em.run.later = function(fn) {
-    laterDefer = Em.RSVP.defer();
-    laterDefer.promise.then(fn);
-  };
-
   Em.run(() => this.$('button').click());
 
   assert.equal(this.$('button').text(), '...please wait...');
 
-  Em.run(() => laterDefer.resolve());
+  this.mockRunLater.next();
+
+  assert.equal(this.$('button').text(), '...continue to wait...');
+
+  this.mockRunLater.next();
 
   assert.equal(this.$('button').text(), 'Click me again');
 
-  Em.run.later = originalRunLater;
-
-  return wait();
+  restoreRunLater(this);
 });
+
+
+//this is brittle and slow, don't use it. It uses a longer `Em.run.later`
+// test('option 1: should change text after a delay', function(assert) {
+//   assert.expect(4);
+//
+//   this.render(hbs`{{delayed-action-button}}`);
+//
+//   assert.equal(this.$('button').text(), 'Click me');
+//
+//   Em.run(() => this.$('button').click());
+//
+//   assert.equal(this.$('button').text(), '...please wait...');
+//
+//   Em.run.later(() => {
+//     assert.equal(this.$('button').text(), '...continue to wait...');
+//   }, 1050);
+//
+//   Em.run.later(() => {
+//     assert.equal(this.$('button').text(), 'Click me again');
+//   }, 2050);
+//
+//   return wait();
+// });
